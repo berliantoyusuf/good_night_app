ad59f8545a94aa9672f84625a4dfc4c814eb8c22
Berlianto Yusuf <berlianto.yusuf@mekari.com>
2024-12-05 22:54:06 +0700

    Modify and update some models

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,12 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+  validates :sleep_time, :wake_time, presence: true
+
+  before_save :calculate_duration
+
+  private
+
+  def calculate_duration
+    self.duration = (wake_time - sleep_time).to_i
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+
+  # Users who follow this user
+  has_many :follower_relationships, class_name: 'Follower', foreign_key: 'following_id', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  # Users this user is following
+  has_many :following_relationships, class_name: 'Follower', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
 end
